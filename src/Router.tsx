c2c2535fa299190595e1af945d3d8ae1986449f7
ujeon <wj9304@gmail.com>
2021-11-25 02:08:54 +0900

    :sparkles: 홈, 설정 탭 추가

---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '@screens/Home';
+import Settings from '@screens/Settings';
+
+const Tab = createBottomTabNavigator();
+
+const Router = function Router() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
