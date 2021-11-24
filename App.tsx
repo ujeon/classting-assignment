e1728d39c5956310681969408502723bdd47b2e3
ujeon <wj9304@gmail.com>
2021-11-24 22:52:07 +0900

    :rotating_light: 린트 에러 수정

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView } from 'react-native';
 
-const App = () => {
+const App = function App() {
   return <SafeAreaView />;
 };
 
