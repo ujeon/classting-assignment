1ddf516e2f0f5cf99383e2826d1a1f77548cae51
ujeon <wj9304@gmail.com>
2021-11-28 20:27:23 +0900

    :bento: 아이콘 추가

--- src/themes/images.ts
+++ src/themes/images.ts
@@ -1,9 +1,11 @@
 import iconPencil from '../assets/icon-pencil.png';
 import iconCancel from '../assets/icon-cancel.png';
+import iconLeftArrow from '../assets/icon-left-arrow.png';
 
 const images = {
   iconPencil,
   iconCancel,
+  iconLeftArrow,
 };
 
 export default images;
