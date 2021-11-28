600f98cba208686bc619e36c1c4ff154df61143f
ujeon <wj9304@gmail.com>
2021-11-28 20:34:33 +0900

    :sparkles: QuizResult 이동 로직 구현

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React, { useCallback, useState } from 'react';
 import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import images from '@themes/images';
 import LeftIconButton from '@components/LeftIconButton';
 import QuizModal from './QuizModal';
 import Quiz from '@store/modules/quiz';
+import { RootStackParamList } from 'Router';
 
-const Home = () => {
+interface NavigationProps {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Home'>;
+}
+
+const Home = ({ navigation }: NavigationProps) => {
   const [quizModalVisible, setQuizModalVisible] = useState(false);
 
   const dispatch = useDispatch();
@@ -20,14 +26,18 @@ const Home = () => {
     setQuizModalVisible((prev) => !prev);
   }, []);
 
-  const handleQuizEnd = useCallback(() => {
-    setQuizModalVisible((prev) => !prev);
-  }, []);
+  const moveQuizResultScreen = useCallback(() => {
+    navigation.navigate('QuizResult');
+  }, [navigation]);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <LeftIconButton source={images.iconPencil} title="퀴즈 시작" onPress={openQuizModal} />
-      <QuizModal visible={quizModalVisible} hideModal={hideQuizModal}></QuizModal>
+      <QuizModal
+        visible={quizModalVisible}
+        hideModal={hideQuizModal}
+        moveQuizResultScreen={moveQuizResultScreen}
+      />
     </View>
   );
 };
