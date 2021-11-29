4427026e293655d228b8d9138227819ca2da7c13
ujeon <wj9304@gmail.com>
2021-11-29 12:11:10 +0900

    :sparkles: Chart 컴포넌트 구현

---
+++ src/components/Chart/styles.ts
@@ -0,0 +1,22 @@
+import colors from '@themes/colors';
+import styled from 'styled-components/native';
+
+export const PieContainer = styled.View`
+  width: 300px;
+  height: 300px;
+`;
+
+export const RatioContainer = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Ratio = styled.Text`
+  color: ${colors.darkGrey};
+  font-size: 35px;
+  font-weight: bold;
+`;
