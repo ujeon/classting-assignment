34145664eae6861998d71c86196867736a121cfe
ujeon <wj9304@gmail.com>
2021-11-29 10:08:56 +0900

    :zap: 이미지 추가

--- src/themes/images.ts
+++ src/themes/images.ts
@@ -1,11 +1,17 @@
 import iconPencil from '../assets/icon-pencil.png';
 import iconCancel from '../assets/icon-cancel.png';
 import iconLeftArrow from '../assets/icon-left-arrow.png';
+import iconTiming from '../assets/icon-timing.png';
+import iconAssignmentCorrect from '../assets/icon-assignment-correct.png';
+import iconAssignmentInCorrect from '../assets/icon-assignment-incorrect.png';
 
 const images = {
   iconPencil,
   iconCancel,
   iconLeftArrow,
+  iconTiming,
+  iconAssignmentCorrect,
+  iconAssignmentInCorrect,
 };
 
 export default images;
