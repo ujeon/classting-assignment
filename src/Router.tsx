47dac7fc40f3f83f7e7c6753e283948eba46131c
ujeon <wj9304@gmail.com>
2021-11-30 16:25:25 +0900

    :fire: 사용하지 않는 파일 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '@screens/Home';
-import Settings from '@screens/Settings';
 import QuizResult from '@screens/QuizResult';
 import QuizRecord from '@screens/QuizRecord';
 import colors from '@themes/colors';
 
 export type RootStackParamList = {
   Home: undefined;
-  Settings: undefined;
   QuizResult: undefined;
   QuizRecord: undefined;
 };
@@ -27,7 +25,6 @@ const Router = () => {
         }}
       >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="QuizResult" component={QuizResult} />
         <Stack.Screen name="QuizRecord" component={QuizRecord} />
       </Stack.Navigator>
